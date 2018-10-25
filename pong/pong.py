4706eb0012c3147f2cc47005b70d2377c14ff351
Robert <robbi111@users.noreply.github.com>
2018-10-25 22:37:23 +0200

    single ball, skeleton functions

--- pong/pong.py
+++ pong/pong.py
@@ -24,20 +24,18 @@ except ImportError as err:
 def main():
     random.seed()
     pygame.init()
+    pygame.display.set_caption('Pong')
     screen = pygame.display.set_mode(SIZE)
-    balls = []
-    for r in (5, 10, 20, 30, 40):
-        balls.append(po.Ball(0, r))
-    # ball = po.Ball(0)
+    ball = po.Ball(0)
     screen.fill(BLACK)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
 
-        for b in balls:
-            screen.blit(b.image, (random.randrange(SIZE[0]),
-                        random.randrange(SIZE[1])))
+        screen.fill(BLACK)
+        screen.blit(ball.image, (random.randrange(SIZE[0]),
+                    random.randrange(SIZE[1])))
         pygame.display.flip()
         pygame.time.delay(1000)
 
--- pong/pong_objects.py
+++ pong/pong_objects.py
@@ -22,5 +22,12 @@ class Ball(pygame.sprite.Sprite):
         # remove black background
         self.image.fill((0, 0, 0))
         self.image.set_colorkey((0, 0, 0))
-        self.image.set_alpha(127)
         pygame.draw.circle(self.image, pygame.Color(255, 255, 255), (r, r), r)
+        self.vector = vector
+        self.area = pygame.display.get_surface()
+
+    def update():
+        return
+
+    def calcNewPos():
+        return
